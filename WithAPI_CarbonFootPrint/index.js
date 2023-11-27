4045e48b3b3becb82399b6aa012054de74f52205
Kaushik Jain <kaushik.jain@intimetec.com>
2023-11-27 20:51:21 +0530

    used api to fetch count

---
+++ WithAPI_CarbonFootPrint/index.js
@@ -0,0 +1,178 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var google = require('googleapis').google;
+var readline = require('readline');
+var fs = require('fs');
+var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+var UserCarbonFootprint = /** @class */ (function () {
+    function UserCarbonFootprint(emailAddress, spamEmailCount, sentEmailCount, inboxEmailCount) {
+        this.inboxCarbonEmissionRate = 0.3;
+        this.sentCarbonEmissionRate = 0.3;
+        this.spamCarbonEmissionRate = 0.03;
+        this.emailAddress = emailAddress;
+        this.spamEmailCount = spamEmailCount;
+        this.sentEmailCount = sentEmailCount;
+        this.inboxEmailCount = inboxEmailCount;
+    }
+    UserCarbonFootprint.prototype.getEmailDomain = function () {
+        var _a;
+        var emailDomain = (_a = this.emailAddress.split('@')[1]) === null || _a === void 0 ? void 0 : _a.split('.')[0];
+        var validDomains = ['gmail', 'outlook', 'yahoo'];
+        if (validDomains.includes(emailDomain)) {
+            return emailDomain;
+        }
+        return 'Invalid Domain';
+    };
+    UserCarbonFootprint.prototype.printUserCarbonFootprint = function () {
+        console.log('-------------OUTPUT-------------');
+        var emailDomain = this.getEmailDomain();
+        if (emailDomain !== 'Invalid Domain') {
+            var inboxCarbonFootprint = (this.inboxEmailCount * this.inboxCarbonEmissionRate) / 1000;
+            var sentCarbonFootprint = (this.sentEmailCount * this.sentCarbonEmissionRate) / 1000;
+            var spamCarbonFootprint = (this.spamEmailCount * this.spamCarbonEmissionRate) / 1000;
+            var totalCarbonFootprint = inboxCarbonFootprint + sentCarbonFootprint + spamCarbonFootprint;
+            console.log('Domain: ' + emailDomain);
+            console.log('Inbox Carbon Footprint: ' + inboxCarbonFootprint + ' KG');
+            console.log('Sent Carbon Footprint: ' + sentCarbonFootprint + ' KG');
+            console.log('Spam Carbon Footprint: ' + spamCarbonFootprint + ' KG');
+            console.log('Total Consumption: ' + totalCarbonFootprint + ' KG');
+        }
+        else {
+            console.log('Invalid email');
+        }
+    };
+    return UserCarbonFootprint;
+}());
+var ServerCarbonFootprint = /** @class */ (function () {
+    function ServerCarbonFootprint(numberOfEmail) {
+        this.numberOfEmail = numberOfEmail;
+    }
+    ServerCarbonFootprint.prototype.printServerCarbonFootprint = function () {
+        console.log('-------------OUTPUT-------------');
+        console.log('Total Server Carbon Footprint: ' + this.numberOfEmail * 0.02 + 'KG');
+    };
+    return ServerCarbonFootprint;
+}());
+function authorize(credentials, callback) {
+    var _a = credentials.web, client_secret = _a.client_secret, client_id = _a.client_id, redirect_uris = _a.redirect_uris;
+    var oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+    var tokenPath = 'token.json';
+    try {
+        var token = fs.readFileSync(tokenPath);
+        oAuth2Client.setCredentials(JSON.parse(token.toString()));
+        callback(oAuth2Client);
+    }
+    catch (error) {
+        getAccessToken(oAuth2Client, function (callback) {
+            console.log(callback);
+        });
+    }
+}
+function getAccessToken(oAuth2Client, callback) {
+    var authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Visit this URL to authorize the app:', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from the page here: ', function (code) {
+        rl.close();
+        oAuth2Client.getToken(code, function (error, token) {
+            if (error) {
+                console.error('Error retrieving access token:', error);
+                return;
+            }
+            oAuth2Client.setCredentials(token);
+            fs.writeFileSync('token.json', JSON.stringify(token));
+            callback(oAuth2Client);
+        });
+    });
+}
+function getEmailLabelCount(gmail, labelId) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            return [2 /*return*/, new Promise(function (resolve, reject) {
+                    gmail.users.labels.get({
+                        userId: 'me',
+                        id: labelId,
+                    }, function (error, res) {
+                        if (error) {
+                            reject("Error retrieving ".concat(labelId, " label: ").concat(error.message));
+                        }
+                        else {
+                            var label = res.data;
+                            resolve(label.messagesTotal);
+                        }
+                    });
+                })];
+        });
+    });
+}
+function fetchAndPrintEmailCarbonFootprint(auth) {
+    return __awaiter(this, void 0, void 0, function () {
+        var gmail, inboxEmailCount, sentEmailCount, spamEmailCount, userCarbonFootprint;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    gmail = google.gmail({ version: 'v1', auth: auth });
+                    return [4 /*yield*/, getEmailLabelCount(gmail, 'INBOX')];
+                case 1:
+                    inboxEmailCount = _a.sent();
+                    return [4 /*yield*/, getEmailLabelCount(gmail, 'SENT')];
+                case 2:
+                    sentEmailCount = _a.sent();
+                    return [4 /*yield*/, getEmailLabelCount(gmail, 'SPAM')];
+                case 3:
+                    spamEmailCount = _a.sent();
+                    userCarbonFootprint = new UserCarbonFootprint('kaushikjain1111@gmail.com', spamEmailCount, sentEmailCount, inboxEmailCount);
+                    userCarbonFootprint.printUserCarbonFootprint();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function CarbonFootprint() {
+    fs.readFile('credentials.json', function (error, credentials) {
+        if (error)
+            return console.error('Error loading client secret file:', error);
+        authorize(JSON.parse(credentials.toString()), fetchAndPrintEmailCarbonFootprint);
+    });
+}
+CarbonFootprint();
---
+++ WithAPI_CarbonFootPrint/index.ts
@@ -0,0 +1,147 @@
+const { google } = require('googleapis');
+const readline = require('readline');
+const fs = require('fs');
+
+const SCOPES: string[] = ['https://www.googleapis.com/auth/gmail.readonly'];
+
+class UserCarbonFootprint {
+  inboxCarbonEmissionRate: number = 0.3;
+  sentCarbonEmissionRate: number = 0.3;
+  spamCarbonEmissionRate: number = 0.03;
+  emailAddress: string;
+  spamEmailCount: number;
+  sentEmailCount: number;
+  inboxEmailCount: number;
+
+  constructor(emailAddress: string, spamEmailCount: number, sentEmailCount: number, inboxEmailCount: number) {
+    this.emailAddress = emailAddress;
+    this.spamEmailCount = spamEmailCount;
+    this.sentEmailCount = sentEmailCount;
+    this.inboxEmailCount = inboxEmailCount;
+  }
+
+  getEmailDomain(): string {
+    const emailDomain = this.emailAddress.split('@')[1]?.split('.')[0];
+    const validDomains: string[] = ['gmail', 'outlook', 'yahoo'];
+    if (validDomains.includes(emailDomain)) {
+      return emailDomain;
+    }
+    return 'Invalid Domain';
+  }
+
+  printUserCarbonFootprint(): void {
+    console.log('-------------OUTPUT-------------');
+    const emailDomain = this.getEmailDomain();
+    if (emailDomain !== 'Invalid Domain') {
+      const inboxCarbonFootprint: number = (this.inboxEmailCount * this.inboxCarbonEmissionRate) / 1000;
+      const sentCarbonFootprint: number = (this.sentEmailCount * this.sentCarbonEmissionRate) / 1000;
+      const spamCarbonFootprint: number = (this.spamEmailCount * this.spamCarbonEmissionRate) / 1000;
+      const totalCarbonFootprint: number = inboxCarbonFootprint + sentCarbonFootprint + spamCarbonFootprint;
+      console.log('Domain: ' + emailDomain);
+      console.log('Inbox Carbon Footprint: ' + inboxCarbonFootprint + ' KG');
+      console.log('Sent Carbon Footprint: ' + sentCarbonFootprint + ' KG');
+      console.log('Spam Carbon Footprint: ' + spamCarbonFootprint + ' KG');
+      console.log('Total Consumption: ' + totalCarbonFootprint + ' KG');
+    } else {
+      console.log('Invalid email');
+    }
+  }
+}
+
+class ServerCarbonFootprint {
+  numberOfEmail: number;
+
+  constructor(numberOfEmail: number) {
+    this.numberOfEmail = numberOfEmail;
+  }
+
+  printServerCarbonFootprint(): void {
+    console.log('-------------OUTPUT-------------');
+    console.log('Total Server Carbon Footprint: ' + this.numberOfEmail * 0.02 + 'KG');
+  }
+}
+
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.web;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+  const tokenPath = 'token.json';
+
+  try {
+    const token = fs.readFileSync(tokenPath);
+    oAuth2Client.setCredentials(JSON.parse(token.toString()));
+    callback(oAuth2Client);
+  } catch (error) {
+    getAccessToken(oAuth2Client, (callback)=>{
+      console.log(callback);
+    });
+  }
+}
+
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+
+  console.log('Visit this URL to authorize the app:', authUrl);
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question('Enter the code from the page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (error, token) => {
+      if (error) {
+        console.error('Error retrieving access token:', error);
+        return;
+      }
+
+      oAuth2Client.setCredentials(token);
+      fs.writeFileSync('token.json', JSON.stringify(token));
+      callback(oAuth2Client);
+    });
+  });
+}
+
+async function getEmailLabelCount(gmail: any, labelId: string): Promise<number> {
+  return new Promise<number>((resolve, reject) => {
+    gmail.users.labels.get({
+      userId: 'me',
+      id: labelId,
+    }, (error: any, res: any) => {
+      if (error) {
+        reject(`Error retrieving ${labelId} label: ${error.message}`);
+      } else {
+        const label = res.data;
+        resolve(label.messagesTotal);
+      }
+    });
+  });
+}
+
+async function fetchAndPrintEmailCarbonFootprint(auth: any): Promise<void> {
+  const gmail = google.gmail({ version: 'v1', auth });
+  const inboxEmailCount: number = await getEmailLabelCount(gmail, 'INBOX');
+  const sentEmailCount: number = await getEmailLabelCount(gmail, 'SENT');
+  const spamEmailCount: number = await getEmailLabelCount(gmail, 'SPAM');
+
+  const userCarbonFootprint = new UserCarbonFootprint(
+    'kaushikjain1111@gmail.com',
+    spamEmailCount,
+    sentEmailCount,
+    inboxEmailCount
+  );
+  userCarbonFootprint.printUserCarbonFootprint();
+}
+
+function CarbonFootprint(): void {
+  fs.readFile('credentials.json', (error, credentials) => {
+    if (error) return console.error('Error loading client secret file:', error);
+
+    authorize(JSON.parse(credentials.toString()), fetchAndPrintEmailCarbonFootprint);
+  });
+}
+
+CarbonFootprint();
